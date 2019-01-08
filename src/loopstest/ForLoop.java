559f2bcd8d0bd26023e673223255691c27c53904
unknown <skoripalli@gmail.com>
2019-01-08 11:48:15 +0000

    first commit

---
+++ src/loopstest/ForLoop.java
@@ -0,0 +1,13 @@
+package loopstest;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int a=1;101>a;a++)
+
+        {
+            System.out.println(+a);
+        }
+    }
+}
+
---
+++ src/loopstest/WhileLoop.java
@@ -0,0 +1,10 @@
+package loopstest;
+public class WhileLoop {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a < 101) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
